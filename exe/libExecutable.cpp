df9f380345703cf6345dfb5185bff59ac8f4a362
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2025-11-05 05:32:16 +0000

    Added cmake project for Linux build

--- exe/libExecutable.cpp
+++ exe/libExecutable.cpp
@@ -1,27 +1,14 @@
-// dllExecutable.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <cassert>
 #include <cmath>
 #include <iostream>
-#include <sstream>
 #include <string>
-#include <windows.h>
 
 #include <fintech_library.h>
 
-void logToVSOutput(const std::string& message) {
-    std::wstring wideMessage(message.begin(), message.end());
-    OutputDebugString(wideMessage.c_str());
-}
+using namespace std;
+
 
 int main() {
-    HWND consoleWindow = GetConsoleWindow(); 
-    ShowWindow(consoleWindow, SW_HIDE);    
-    OutputDebugString(L"Console window hidden.\n");
-
-    std::ostringstream oss;
-
     const double calculatedPrice01 = std::round(
         PriceEuropeanOption(
             /*type=*/Call,
@@ -34,8 +21,7 @@ int main() {
             /*strike_price=*/ 105
         ) * 100.0) / 100;
     assert(calculatedPrice01 == 16.44);
-    oss << "The calculated price for a Call with the BS method is: " << calculatedPrice01 << "\n";
-    logToVSOutput(oss.str()); oss.str("");
+    cout << "The calculated price for a Call with the BS method is: " << calculatedPrice01 << endl;
 
     const double calculatedPrice02 = std::round(
         PriceEuropeanOption(
@@ -49,8 +35,7 @@ int main() {
             /*strike_price=*/ 105
         ) * 100.0) / 100;
     assert(calculatedPrice02 == 17.35);
-    oss << "The calculated price for a Put with the Binomial method is: " << calculatedPrice02 << "\n";
-    logToVSOutput(oss.str()); oss.str("");
+    cout << "The calculated price for a Put with the Binomial method is: " << calculatedPrice02 << endl;
     
     const double market_price = 30.95;
     const double impliedVolatiliy = std::round(
@@ -63,10 +48,7 @@ int main() {
             /*initial_share_price=*/ 227.5
         ) * 100.0) / 100;
     assert(impliedVolatiliy == 0.29);
-    oss.clear();
-    oss << "The implied volatily for a Call with market price " << market_price << " is: " << impliedVolatiliy << "\n";
-    logToVSOutput(oss.str());
+    cout << "The implied volatily for a Call with market price " << market_price << " is: " << impliedVolatiliy << endl;
 
-    system("pause");
     return 0;
 }
